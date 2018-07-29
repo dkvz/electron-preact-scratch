9c488be4e95f17b40c8a31ee8a9f816692367cdf
DkVZ <bouc@clanz.be>
2018-07-29 15:50:16 +0200

    Confirmed it would work from outside the component

--- src/vue1.js
+++ src/vue1.js
@@ -1,13 +1,23 @@
 const { h, render, Component } = require('preact');
 const Editor = require('../dist/editor');
+const renderer = require('../renderer');
 
 class Vue1 extends Component {
+
+  trySaving() {
+    renderer.sendSaveObj();
+  }
 
   render() {
     return (
       <div>
         <Editor />
-        <div style={{'background-color':'orange', padding: '20px'}}>STATUSBAR lol</div>
+        <div style={{'background-color':'orange', padding: '20px'}}>
+          STATUSBAR lol
+          <a href="#" style={{'float':'right'}} onClick={this.trySaving}>
+            Save
+          </a>
+        </div>
       </div>
     );
   }
